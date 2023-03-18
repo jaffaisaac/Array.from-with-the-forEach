8cab8707923a4f55ea8bcff53a57ccd7d7ba94fa
jaffa Isaac <jaffaisaac4@gmail.com>
2023-03-18 14:40:14 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const customers ={
+    '0':`oranges`,
+    '1':`Cocking Oil`,
+    '2':`bananas`,
+    '3':`bread`,
+    '4':`books`,
+    '5':`computer`,
+    length:6
+}
+
+ Array.from(customers).forEach(customer=>{
+  console.log(customer);
+ })
